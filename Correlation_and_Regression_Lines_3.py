f25c8bcb042f9a077c7d6cd63c28f1ac74eaebfe
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2020-04-29 09:22:45 +0530

    Update Correlation_and_Regression_Lines_3.py

--- Correlation_and_Regression_Lines_3.py
+++ Correlation_and_Regression_Lines_3.py
@@ -1,11 +1,11 @@
 p=[15,12,8,8,7,7,7,6,5,3]
 h=[10,25,17,11,13,17,20,13,9,15]
 n = len(p)
-x_y = sum([i*j for (i,j) in zip(p,h)])
+xy = sum([i*j for (i,j) in zip(p,h)])
 x = sum(p)
 y = sum(h)
-x_x = sum([i**2 for i in p])
-a = (y*x_x - x*x_y) / (n*x_x - x**2)
-b = (n*x_y - x*y) / (n*x_x - x**2)
+x2 = sum([i**2 for i in p])
+a = (y*x2 - x*xy) / (n*x2 - x**2)
+b = (n*xy - x*y) / (n*x2 - x**2)
 ans = a + b*10
 print(round(ans,1))
