b54168762744f1ab41a1b09650bcb0d4e53368d2
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2020-09-19 19:17:09 +0530

    Delete Standard_Deviation_Puzzles.py

--- Statistics-and-Machine-Learning/Standard_Deviation_Puzzles.py
+++
@@ -1,5 +0,0 @@
-#mean is 0.675
-#standard deviation is 0.065m
-#height of one child is 90.25cm
-#find the number of standard deviations away from the mean that the particular child is.
-print(round((.9025-.675) / .065,2))
